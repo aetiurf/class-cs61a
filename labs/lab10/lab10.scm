7e18e44c7e98f2b12f642de56f2a9e284394c0c1
fruitea <2500901603@qq.com>
2022-05-12 14:31:09 +0800

    lab10 completed...

---
+++ labs/lab10/lab10.scm
@@ -0,0 +1,131 @@
+(define (over-or-under num1 num2)
+  'YOUR-CODE-HERE
+  (if (= num1 num2)
+    0
+    (if (< num1 num2)
+        -1
+        1
+    )
+  )
+)
+
+;;; Tests
+(over-or-under 1 2)
+; expect -1
+(over-or-under 2 1)
+; expect 1
+(over-or-under 1 1)
+; expect 0
+
+
+(define (filter-lst fn lst)
+  'YOUR-CODE-HERE
+  (if (null? lst)
+    nil
+    (if (fn (car lst))
+        (cons 
+            (car lst) 
+            (filter-lst fn (cdr lst))
+        )
+        (filter-lst fn (cdr lst))
+    )
+  )
+)
+
+;;; Tests
+(define (even? x)
+  (= (modulo x 2) 0))
+(filter-lst even? '(0 1 1 2 3 5 8))
+; expect (0 2 8)
+
+
+(define (make-adder num)
+  'YOUR-CODE-HERE
+  (lambda (inc) (+ num inc))
+)
+
+;;; Tests
+(define adder (make-adder 5))
+(adder 8)
+; expect 13
+
+
+(define lst
+  ;'YOUR-CODE-HERE
+  '((1) 2 (3 4) 5)
+)
+
+
+(define (composed f g)
+  'YOUR-CODE-HERE
+  (lambda (x) (f (g x)))
+)
+
+
+(define (remove item lst)
+  'YOUR-CODE-HERE
+  (filter-lst 
+    (lambda (x) 
+        (if (= item x)
+            #f #t
+        )
+    )
+    lst
+  )
+)
+
+
+;;; Tests
+(remove 3 nil)
+; expect ()
+(remove 3 '(1 3 5))
+; expect (1 5)
+(remove 5 '(5 3 5 5 1 4 5 4))
+; expect (3 1 4 4)
+
+
+(define (no-repeats s)
+  'YOUR-CODE-HERE
+    (if (null? s)
+        nil
+        (cons
+            (car s)
+            (remove 
+                (car s) 
+                (no-repeats (cdr s))
+            )
+        )
+    )
+)
+
+
+(define (substitute s old new)
+  'YOUR-CODE-HERE
+    (if (null? s)
+        nil
+        (cons
+            (if (pair? (car s))
+                (substitute (car s) old new)
+                (if (eq? (car s) old)
+                    new
+                    (car s)
+                )
+            )
+            (substitute (cdr s) old new)
+        )
+    )
+)
+
+
+(define (sub-all s olds news)
+  'YOUR-CODE-HERE
+    (if (null? olds)
+        s
+        (sub-all 
+            (substitute s (car olds) (car news))
+            (cdr olds) 
+            (cdr news)
+        )
+    )
+)
+
